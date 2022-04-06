d0863333ffc577aa44bd052b9e9df13277439f33
ISC.Safarian <ISC.Safarian@cbi.ir>
2022-04-06 15:23:48 +0430

    downgrading tribe react-sdk

--- src/pages/recent-posts/[slug]/index.tsx
+++ src/pages/recent-posts/[slug]/index.tsx
@@ -4,28 +4,35 @@ import { usePost } from '@tribeplatform/react-sdk/hooks'
 function SinglePost() {
     const router = useRouter()
     const postid = router.query.pid as string
-    const {data: post} = usePost({variables: {id: postid}})
-
+    const {data: post, isLoading} = usePost({id: postid})
     console.log(post)
-    return (
-        <div className="card">
-            {postid}
-            {/* <div className="card-header py-10">
-                <div className="row align-items-center">
-                    <div className="col-2">
-                        <img className="img w-100 rounded-circle" src={props.post.owner.member.profilePicture.url} />
-                    </div>
-                    <div className="col-10">
-                        <a><h1 className="fs-2x text-dark">{props.post.title}</h1></a>
-                        <div className="fs-5 text-muted fw-bold">{props.post.owner.member.name}, {props.post.owner.member.tagline}</div>
+    if (isLoading) {
+        return (
+            <div className="card">
+                <p className="fw-bold">Loading...</p>
+            </div>
+        )
+    }
+    else {
+        return (
+            <div className="card">
+                {isLoading && <p className="fw-bold">Loading...</p>}
+                {!isLoading}
+                <div className="card-header py-10">
+                    <div className="row align-items-center">
+                        <a><h1 className="fs-2x text-dark">{post?.title}</h1></a>
+                        <div className="fs-5 text-muted fw-bold">{post?.owner.member.name}, {post?.owner.member.tagline}</div>
                     </div>
                 </div>
+                <div className="card-body p-5 px-lg-10 py-lg-10">
+                    <div className="row g-10" dangerouslySetInnerHTML={{ __html: JSON.parse(post?.mappingFields[1].value) }}></div>
+                </div>
+                <div className="card-footer">
+                    <button type="button" className="btn btn-warning">Like & Get Rewarded</button>
+                </div>
             </div>
-            <div className="card-body p-5 px-lg-10 py-lg-10">
-                <div className="row g-10" dangerouslySetInnerHTML={{ __html: props.post.shortContent }}></div>
-            </div> */}
-        </div>
-    );
+        )
+    }
 }
 
 export default SinglePost;
